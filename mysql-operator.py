b6218533f605b9aa39923c2f703107aa13f6ed4c
Evgeny Nikitin <evgenikk96@gmail.com>
2019-09-15 02:49:31 +0300

    add on delete and job logic

--- mysql-operator.py
+++ mysql-operator.py
@@ -1,47 +1,88 @@
 import kopf
 import yaml
 import kubernetes
+from kubernetes import client
 from jinja2 import Environment, FileSystemLoader
+
+
+# def wait_until_job_end(jobname):
+#     api = kubernetes.client.BatchV1Api()
+#     jobs = api.list_namespaced_job('default')
+#     for job in jobs.items:
+#         if job == jobname:
+#             while job.status.active == 1:
+#                 pass
+
+
+def delete_success_jobs(mysql_instance_name):
+    print("start deletion")
+    api = kubernetes.client.BatchV1Api()
+    jobs = api.list_namespaced_job('default')
+    for job in jobs.items:
+        jobname = job.metadata.name
+        print('iterate: jobname')
+        if (jobname == f"backup-{mysql_instance_name}-job") or \
+                (jobname == f"restore-{mysql_instance_name}-job"):
+            print('firs if passed')
+            if job.status.succeeded == 1:
+                print(f"Cleaning up Job: {jobname} Finished at: {job.status.completion_time}")
+                api.delete_namespaced_job(jobname,
+                                          'default',
+                                          propagation_policy='Background')
+
+                print("object must be deleted")
+
 
 def render_template(filename, vars_dict):
     env = Environment(loader=FileSystemLoader('./templates'))
     template = env.get_template(filename)
-    yaml_manifest = template.render( vars_dict )
+    yaml_manifest = template.render(vars_dict)
     json_manifest = yaml.load(yaml_manifest)
     return(json_manifest)
 
-@kopf.on.create('otus.homework', 'v1', 'mysqls') 
-def mysql_on_create(body, spec, **kwargs): # Функция, которая будет запускаться при создании объектов тип MySQL
-    name = body['metadata']['name']  
+
+@kopf.on.create('otus.homework', 'v1', 'mysqls')
+# Функция, которая будет запускаться при создании объектов тип MySQL:
+def mysql_on_create(body, spec, **kwargs):
+    name = body['metadata']['name']
     image = body['spec']['image']
     password = body['spec']['password']
     database = body['spec']['database']
     storage_size = body['spec']['storage_size']
 
-
-
     # Генерируем JSON манифесты для деплоя
-    persistent_volume = render_template('mysql-pv.yml.j2',{'name': name, 'storage_size': storage_size})
-    persistent_volume_claim = render_template('mysql-pvc.yml.j2',{'name': name, 'storage_size': storage_size })
-    service = render_template('mysql-service.yml.j2',{'name': name})
+    persistent_volume = render_template('mysql-pv.yml.j2',
+                                        {'name': name,
+                                         'storage_size': storage_size})
+    persistent_volume_claim = render_template('mysql-pvc.yml.j2',
+                                              {'name': name,
+                                               'storage_size': storage_size})
+    service = render_template('mysql-service.yml.j2', {'name': name})
 
-    deployment = render_template('mysql-deployment.yml.j2',{
-    'name': name,
-    'image': image, 
-    'password': password, 
-    'database': database })
+    deployment = render_template('mysql-deployment.yml.j2', {
+        'name': name,
+        'image': image,
+        'password': password,
+        'database': database})
+
+    restore_job = render_template('restore-job.yml.j2', {
+        'name': name,
+        'image': image,
+        'password': password,
+        'database': database})
+
     # Определяем, что созданные ресурсы являются дочерними к управляемому CustomResource:
-    kopf.adopt(persistent_volume, owner=body)
-    kopf.adopt(persistent_volume_claim, owner=body)
-    kopf.adopt(service, owner=body)
-    kopf.adopt(deployment, owner=body)
-    # ^ Таким образом при удалении CR удалятся все, связанные с ним pv,pvc,svc,deployments
+    kopf.append_owner_reference(persistent_volume, owner=body)
+    kopf.append_owner_reference(persistent_volume_claim, owner=body)  # addopt
+    kopf.append_owner_reference(service, owner=body)
+    kopf.append_owner_reference(deployment, owner=body)
+    # ^ Таким образом при удалении CR удалятся все, связанные с ним pv,pvc,svc, deployments
     api = kubernetes.client.CoreV1Api()
 
     # Создаем mysql PV:
     api.create_persistent_volume(persistent_volume)
     # Создаем mysql PVC:
-    api.create_namespaced_persistent_volume_claim('default', persistent_volume)
+    api.create_namespaced_persistent_volume_claim('default', persistent_volume_claim)
     # Создаем mysql SVC:
     api.create_namespaced_service('default', service)
 
@@ -49,8 +90,46 @@ def mysql_on_create(body, spec, **kwargs): # Функция, которая бу
     api = kubernetes.client.AppsV1Api()
     api.create_namespaced_deployment('default', deployment)
 
+    # Пытаемся восстановиться из backup
+    try:
+        api = kubernetes.client.BatchV1Api()
+        api.create_namespaced_job('default', restore_job)
+    except kubernetes.client.rest.ApiException as e:
+        print("Exception when creation restore job: %s\n" % e)
+
+    # Cоздаем PVC  и PV для бэкапов:
+    try:
+        backup_pv = render_template('backup-pv.yml.j2', {'name': name})
+        api = kubernetes.client.CoreV1Api()
+        print(api.create_persistent_volume(backup_pv))
+        api.create_persistent_volume(backup_pv)
+    except kubernetes.client.rest.ApiException:
+        print('backup pv was not created')
+
+    try:
+        backup_pvc = render_template('backup-pvc.yml.j2', {'name': name})
+        api = kubernetes.client.CoreV1Api()
+        api.create_namespaced_persistent_volume_claim('default', backup_pvc)
+    except kubernetes.client.rest.ApiException:
+        print('backup pvc was not created')
 
 
 @kopf.on.delete('otus.homework', 'v1', 'mysqls')
-def delete(body, **kwargs):
+def delete_object_make_backup(body, **kwargs):
+    name = body['metadata']['name']
+    image = body['spec']['image']
+    password = body['spec']['password']
+    database = body['spec']['database']
+
+    delete_success_jobs(name)
+
+    # Cоздаем backup job:
+    api = kubernetes.client.BatchV1Api()
+    backup_job = render_template('backup-job.yml.j2', {
+        'name': name,
+        'image': image,
+        'password': password,
+        'database': database})
+    api.create_namespaced_job('default', backup_job)
+    delete_success_jobs(name)
     return {'message': "mysql and its children resources deleted"}
